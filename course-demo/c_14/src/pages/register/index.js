10ee8c4525ed3e8f84974d4f04d62242cd4adc6a
Ken Huang <bluetch@gmail.com>
2022-09-18 17:38:08 +0800

    c_14

--- course-demo/c_14/src/pages/register/index.js
+++ course-demo/c_14/src/pages/register/index.js
@@ -3,63 +3,68 @@ import { Button, Layout } from '../../components';
 import styles from './register.module.scss';
 
 export const Register = () => {
-  const [nameError, setNameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [mobileError, setMobileError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [passwordAgainError, setpasswordAgainError] = useState(false);
-
   const nameRef = useRef();
   const emailRef = useRef();
   const mobileRef = useRef();
   const passwordRef = useRef();
   const passwordAgainRef = useRef();
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-    const nameRegex = /\w/g;
-    const emailRegex = /[a-z0-9]@[a-z]+\.[a-z]{2,3}/g;
-    const mobileRegex = /\d{10}/g;
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g;
+  const nameRegex = /\w/g;
+  const emailRegex = /[a-z0-9]@[a-z]+\.[a-z]{2,3}/g;
+  const mobileRegex = /\d{10}/g;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g;
 
-    // 判斷 full name是否符合格式
-    if (nameRegex.test(nameRef.current.value)) {
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [mobileError, setMobileError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordAgainError, setPasswordAgainError] = useState(false);
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    if(nameRegex.test(nameRef.current.value)){
       setNameError(false);
-    } else {
+    }else{
       setNameError(true);
+      return true;
     }
 
-    // 判斷 email是否符合格式
-    if (emailRegex.test(emailRef.current.value)) {
+    if(emailRegex.test(emailRef.current.value)){
       setEmailError(false);
-    } else {
+    }else{
       setEmailError(true);
+      return true;
     }
 
-    // 判斷手機號碼是否符合格式
-    if (mobileRegex.test(mobileRef.current.value)) {
+    if(mobileRegex.test(mobileRef.current.value)){
       setMobileError(false);
-    } else {
+    }else{
       setMobileError(true);
+      return true;
     }
 
-    // 判斷密碼格式是否夠安全
-    if (passwordRegex.test(passwordRef.current.value)) {
+    if(passwordRegex.test(passwordRef.current.value)){
       setPasswordError(false);
-    } else {
+    }else{
       setPasswordError(true);
+      return true;
     }
 
-    // 判斷密碼內容是否一樣
-    if (passwordRef.current.value === passwordAgainRef.current.value) {
-      setpasswordAgainError(false);
-    } else {
-      setpasswordAgainError(true);
+    if(passwordRef.current.value === passwordAgainRef.current.value){
+      setPasswordAgainError(false);
+    }else{
+      setPasswordAgainError(true);
+      return;
+    }
+
+    if(!nameError && !mobileError && !mobileError && !passwordError && !passwordAgainError){
+      // all false
+      window.location.href="/";
     }
   }
 
   useEffect(() => {
-    document.title = "Homework";
+    // function
   }, []);
 
   return (
@@ -68,35 +73,30 @@ export const Register = () => {
         <form action="" className={styles.form}>
           <h1>Create Account</h1>
           <label htmlFor="name">
-            <span>Full Name</span>
-            <input type="text" id="name" ref={nameRef} onBlur={() => console.log('blur')}/>
-            {nameError && <div className={styles.error}>格式錯誤，請使用英數字</div>}
+            <span>Full name</span>
+            <input type="text" id="name" ref={nameRef} placeholder="請輸入你的英文名字" />
+            {nameError && (<div className={styles.error}>名字格式錯誤，請使用英文文字</div>)}
           </label>
-
           <label htmlFor="email">
             <span>Email</span>
-            <input type="text" id="email" ref={emailRef}/>
-            {emailError && <div className={styles.error}>Email格式錯誤</div>}
+            <input type="text" id="email" ref={emailRef} placeholder="e.g yourname@gmail.com"/>
+            {emailError && (<div className={styles.error}>Email格式錯誤</div>)}
           </label>
-
           <label htmlFor="mobile">
-            <span>Mobile Number</span>
-            <input type="text" id="mobile" ref={mobileRef}/>
-            {mobileError && <div className={styles.error}>手機號碼格式錯誤，請輸入純數字</div>}
+            <span>mobile Number</span>
+            <input type="text" id="mobile" ref={mobileRef} />
+            {mobileError && (<div className={styles.error}>手機格式錯誤，請使用純數字</div>)}
           </label>
-
           <label htmlFor="password">
-            <span>Password</span>
-            <input type="password" id="password" ref={passwordRef}/>
-            {passwordError && <div className={styles.error}>請輸入最少8字元，包含一個以上英文及數字</div>}
+            <span>password</span>
+            <input type="password" id="password" ref={passwordRef} />
+            {passwordError && (<div className={styles.error}>密碼格式錯誤，請最少輸入8碼，包括至少一個英文和數字</div>)}
           </label>
-
           <label htmlFor="passwordAgain">
-            <span>Confirm Password</span>
-            <input type="password" id="passwordAgain" ref={passwordAgainRef}/>
-            {passwordAgainError && <div className={styles.error}>必須與上面輸入的密碼一致</div>}
+            <span>confirm password</span>
+            <input type="password" id="passwordAgain" ref={passwordAgainRef} />
+            {passwordAgainError && (<div className={styles.error}>必須與上面輸入的密碼一致</div>)}
           </label>
-
           <Button onClick={handleRegister}>Register</Button>
         </form>
       </div>
